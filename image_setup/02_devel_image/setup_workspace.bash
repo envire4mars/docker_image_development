9e5465e9b02632393f1498bbf27e208b4b1b84fa
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2021-09-29 20:49:16 +0200

    edit settings and setup_workspace to init corobx project

--- image_setup/02_devel_image/setup_workspace.bash
+++ image_setup/02_devel_image/setup_workspace.bash
@@ -5,36 +5,38 @@
 # stop on errors
 set -e
 
-BUILDCONF=
-BRANCH=
+BUILDCONF=https://git.hb.dfki.de/corob-x/buildconf.git
+BRANCH=master
 
 # In this file you can add a script that intitializes your workspace
 
 # ROCK BUILDCONF EXAMPLE
 #
-#if [ ! -f /opt/workspace/env.sh ]; then
-#    echo -e "\e[32m[INFO] First start: setting up the workspace.\e[0m"
-#
-#    # go to workspace dir
-#    cd /opt/workspace/
-#
-#    # set git config
-#    git config --global user.name "Image Builder"
-#    git config --global user.email "image@builder.me"
-#    git config --global credential.helper cache
-#
-#    # setup ws using autoproj
-#    wget rock-robotics.org/autoproj_bootstrap
-#    ruby autoproj_bootstrap git $BUILDCONF branch=$BRANCH
-#    source env.sh
-#    aup
-#    amake
-#
-#    echo -e "\e[32m[INFO] workspace successfully initialized.\e[0m"
-#else 
-#    echo -e "\e[31m[ERROR] workspace already initialized.\e[0m"
-#    exit 1
-#fi
+if [ ! -f /opt/workspace/env.sh ]; then
+    echo -e "\e[32m[INFO] First start: setting up the workspace.\e[0m"
+
+    sudo apt update
+
+    # go to workspace dir
+    cd /opt/workspace/
+
+    # set git config
+    git config --global user.name "Image Builder"
+    git config --global user.email "image@builder.me"
+    git config --global credential.helper cache
+
+    # setup ws using autoproj
+    wget rock-robotics.org/autoproj_bootstrap
+    ruby autoproj_bootstrap git $BUILDCONF branch=$BRANCH
+    source env.sh
+    aup
+    amake
+
+    echo -e "\e[32m[INFO] workspace successfully initialized.\e[0m"
+else 
+    echo -e "\e[31m[ERROR] workspace already initialized.\e[0m"
+    exit 1
+fi
 
 # ROS BUILDCONF EXAMPLE
 #
