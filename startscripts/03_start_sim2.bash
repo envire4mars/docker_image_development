e0f74e49f1a425b1536f1f90fbb0b360b7095b58
Malte Langosz <malte.langosz@dfki.de>
2021-10-06 14:11:37 +0200

    Update to release image as devault

--- startscripts/03_start_sim2.bash
+++ startscripts/03_start_sim2.bash
@@ -18,7 +18,7 @@ echo "Restarting omniorb nameserver"
 bash /opt/workspace/tools/cnd/orogen/execution/systemd/omniorb_restart.sh
 echo "Selecting bundle $BUNDLE"
 rock-bundle-sel $BUNDLE
-echo "Starting rock_runtime_simple.rb"
+echo "Starting rock_runtime"
 #ruby /opt/workspace/bundles/sherpa_tt_corobx/scripts/rock-runtime-simple.rb
 rock-runtime &
 
