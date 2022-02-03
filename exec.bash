a85dc976e364039b8717ec1b901c1b33d0920422
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2022-02-03 11:33:44 +0100

    make xhost command conditional

--- exec.bash
+++ exec.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #allow local connections of root (docker daemon) to the current users x server
-xhost +local:root > /dev/null
+if command -v xhost > /dev/null; then
+    xhost +local:root > /dev/null
+fi
 
 ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $ROOT_DIR/docker_commands.bash
@@ -118,4 +120,6 @@ DOCKER_RUN_ARGS=" \
 init_docker $@
 
 #remove permission for local connections of root (docker daemon) to the current users x server
-xhost -local:root > /dev/null
+if command -v xhost > /dev/null; then
+    xhost -local:root > /dev/null
+fi
