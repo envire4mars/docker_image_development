fb1a835faa19b6bfe389839ceacc988daf41f662
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2022-02-07 18:47:03 +0100

    Rework config_seed handling to avoid divergence

--- image_setup/02_devel_image/setup_workspace.bash
+++ image_setup/02_devel_image/setup_workspace.bash
@@ -27,10 +27,10 @@ if [ ! -f /opt/workspace/env.sh ]; then
 
     # setup ws using autoproj
     wget rock-robotics.org/autoproj_bootstrap
-    ruby autoproj_bootstrap git $BUILDCONF branch=$BRANCH --seed-config=/opt/corobx_seed_config.yml
+    wget -q https://git.hb.dfki.de/corob-x/buildconf/-/raw/$BRANCH/config_seed.yml
+    ruby autoproj_bootstrap git $BUILDCONF branch=$BRANCH --seed-config=config_seed.yml
     source env.sh
     aup
- #   export ROCK_BUNDLE="/opt/workspace/bundles/sherpa_tt_corobx"
     amake
 
     echo -e "\e[32m[INFO] workspace successfully initialized.\e[0m"
@@ -38,34 +38,3 @@ else
     echo -e "\e[31m[ERROR] workspace already initialized.\e[0m"
     exit 1
 fi
-
-# ROS BUILDCONF EXAMPLE
-#
-## add /opt/startscritps to path, you need to do it here, because /home/devel is a mounted folder
-#echo 'export PATH=${PATH}:/opt/startscripts' | sudo tee -a /home/devel/.bashrc > /dev/null
-#if [ ! -d /opt/workspace/src ]; then
-#    echo "first start: setting up workspace"
-#    mkdir -p /opt/workspace/src
-#    cd /opt/workspace/
-#    #source /opt/setup_env.sh
-#    source /opt/ros/melodic/setup.bash
-#    catkin init && catkin build
-#
-#    echo "[INFO] Setting up workspace with autoproj."
-#    cd /opt/workspace/src
-#    wget https://rock-robotics.org/autoproj_bootstrap
-#    git config --global user.name "Image Builder"
-#    git config --global user.email "image@builder.me"
-#    git config --global credential.helper cache
-#    ruby autoproj_bootstrap git $BUILDCONF branch=$BRANCH
-#    . env.sh
-#    aup
-#    cd /opt/workspace
-#    echo
-#    echo "workspace initialized, please"
-#    echo "source devel/setup.bash"
-#    echo "catkin build"
-#    echo
-#else
-#    echo "[ERROR] Workspace is already initialized (/opt/workspace/src already exists)."
-#fi
